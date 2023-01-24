41d6958688f1dc7b1e06aada383b21fb825ed5d3
Skelton <mskelton1@una.edu>
2023-01-24 16:41:12 -0600

    set up ejs

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+let myName = 'Meghan';
+
 app.get('/', (req, res) => {
-  res.send('Hello World- is it November?')
+  //res.send('Hello World- is it November?')
+  res.send(`<h3>Hi, ${ myName }</h3>`)
+
 })
 
 app.get('/show', (req, res) => {
     //res.sendFile('index.html');
-
     res.sendFile('index.html' , { root : __dirname});
+})
+
+app.get('/ejs', (req,res) => {
+    //ejs stuff goes here
+    res.render('index');
 })
 
 console.log('in the node console');
