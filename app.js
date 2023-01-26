5b084a207facc1754793ef36a02f918af7adc82a
Skelton <mskelton1@una.edu>
2023-01-26 16:44:11 -0600

    ejs form and client stuff /cant remember all of it

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.set('view engine', 'ejs');
 
 let myName = 'Meghan';
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   //res.send('Hello World- is it November?')
-  res.send(`<h3>Hi, ${ myName }</h3>`)
+  let result = await res.send(`<h3>Hi, ${ myName }</h3>`);
+
+  console.log( myName);
 
 })
 
@@ -17,10 +19,22 @@ app.get('/show', (req, res) => {
     res.sendFile('index.html' , { root : __dirname});
 })
 
+console.log("before app dot get to slash ejs", myName);
+
 app.get('/ejs', (req,res) => {
     //ejs stuff goes here
-    res.render('index');
+    console.log("in /ejs before render:", myName);
+    
+    res.render('index', { myName: myName } ); //left one is ejs, right is node
+    
+    console.log("after res render /ejs", myName);
 })
+
+app.get('/name', (req,res) => {
+  console.log("in get to slash name:", req.query.ejsFormName);    
+})
+
+console.log("after app dot get to slash ejs", myName);
 
 console.log('in the node console');
 
